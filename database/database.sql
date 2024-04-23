8468f7a9dab77084ce0ec82be5769b68f50d4424
piyushparmar911 <prparmar911@gmail.com>
2024-04-23 18:18:48 +0530

    database complete

--- database/database.sql
+++ database/database.sql
@@ -1,7 +1,93 @@
-DROP TABLE IF EXISTS `STUDMANAGE`;
+CREATE DATABASE `ManagementDb`;
+
+USE `ManagementDb`;
+CREATE TABLE
+    `Modules` (
+        `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        `Name` VARCHAR(255) NOT NULL
+    );
+
+CREATE TABLE
+    `Roles` (
+        `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        `Name` VARCHAR(255),
+        `ModuleId` INT NOT NULL,
+        FOREIGN KEY (`ModuleId`) REFERENCES `Modules` (`Id`)
+    );
+
+CREATE TABLE
+    `Users` (
+        `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        `RoleId` INT NOT NULL,
+        `Subject` VARCHAR(255) NOT NULL,
+        `Address` VARCHAR(255) NOT NULL,
+        `Password` VARCHAR(255) NOT NULL,
+        `Expericance` INT NOT NULL,
+        `ExpericanceGain` INT NOT NULL,
+        `LastDegree` VARCHAR(255) NOT NULL,
+        `LastWork` VARCHAR(255) NOT NULL,
+        `Salary` INT NOT NULL,
+        FOREIGN key (`RoleId`) REFERENCES `Roles` (`Id`)
+    );
 
 
-CREATE DATABASE `STUDMANAGE`;
+CREATE TABLE
+    `Permissions` (
+        `ADD` BOOLEAN,
+        `Delete` BOOLEAN,
+        `Select` BOOLEAN,
+        `View` BOOLEAN,
+        `ModuleId` INT NOT NULL,
+        `UserId` INT NOT NULL,
+        FOREIGN KEY (`UserId`) REFERENCES `Users` (`Id`),
+        FOREIGN KEY (`ModuleId`) REFERENCES `Modules` (`Id`)
+    );
 
+CREATE TABLE
+    `Class` (
+        `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        `Name` VARCHAR(255) NOT NULL,
+        `Boys` INT NOT NULL,
+        `Girls` INT NOT NULL,
+        `TotalStudents` INT NOT NULL
+    );
 
-USE `STUDMANAGE`;
+CREATE TABLE
+    `ClassAllodate` (`Id` INT NOT NULL, `Class` INT NOT NULL);
+
+CREATE TABLE
+    `Subjects` (
+        `Id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        `ClassId` INT NOT NULL,
+        `Name` VARCHAR(255) NOT NULL,
+        FOREIGN KEY (`ClassId`) REFERENCES `Class` (`Id`)
+    );
+
+CREATE TABLE
+    `SubjectAllodate` (
+        `Id` INT NOT NULL,
+        `UserId` INT NOT NULL,
+        `SubjectId` INT NOT NULL,
+        FOREIGN KEY (`UserId`) REFERENCES `Users` (`Id`),
+        FOREIGN KEY (`SubjectId`) REFERENCES `Subjects` (`Id`)
+    );
+
+CREATE TABLE
+    `Event` (
+        `Id` INT NOT NULL,
+        `UserId` INT NOT NULL,
+        `Name` VARCHAR(255) NOT NULL,
+        `DateTime` DATE,
+        FOREIGN KEY (`UserId`) REFERENCES `Users` (`Id`)
+    );
+
+CREATE TABLE
+    `Leave` (
+        `Id` INT NOT NULL,
+        `UserId` INT NOT NULL,
+        `DateStart` DATE NOT NULL,
+        `DateEnd` DATE NOT NULL,
+        `Reason` VARCHAR(255) NOT NULL,
+        `Status` VARCHAR(255) NOT NULL,
+        FOREIGN KEY (`UserId`) REFERENCES `Users` (`Id`)
+    );
