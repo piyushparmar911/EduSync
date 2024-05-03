7950ae9e4f3ef7d882ef466733c22225744ef2c2
piyushparmar911 <prparmar911@gmail.com>
2024-05-03 16:56:39 +0530

    sidebar page updated

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -8,23 +8,23 @@
                         <span>Main Menu</span>
                      </li>
                      <li class="submenu active">
-                        <a href="#"><i class="fas fa-user-graduate"></i> <span> Dashboard</span> <span class="menu-arrow"></span></a>
+                        <a href="#"><h5 class="fas fa-user-graduate"></h5> <span> <h5>Dashboard</h5></span> <span class="menu-arrow"></span></a>
                         <ul>
-                           <li><a href="<?=urlOf("index.php")?>" class="active" accesskey="a">Admin Dashboard</a></li>
-                           <li><a href="<?=urlOf("pages/users/user-dashboard.php")?>">User Dashboard</a></li>
+                           <li><a href="<?=urlOf("index.php")?>" class="active" accesskey="a"><h6>Admin Dashboard</h6></a></li>
+                           <li><a href="<?=urlOf("pages/users/user-dashboard.php")?>"><h6>User Dashboard</h6></a></li>
                         </ul>
                         
                      <li class="users">
-                        <a href="<?=urlOf("pages/users/user-list.php")?>" accesskey="u"><i class="fas fa-users"></i> <span> Users</span></a>
+                        <a href="<?=urlOf("pages/users/user-list.php")?>" accesskey="u"><h5 class="fas fa-users"></h5> <span><h5>Users</h5></span></a>
                      </li>
                      <li class="Departments">
-                        <a href="<?=urlOf("pages/department/departments-list.php")?>" accesskey="p"><i class="fas fa-building"></i><span>Departments </span></a>
+                        <a href="<?=urlOf("pages/department/departments-list.php")?>" accesskey="p"><h5 class="fas fa-building"></h5><span><h5>Departments</h5> </span></a>
                      </li>
                      <li class="Subjects">
-                        <a href="<?=urlOf("pages/subjects/subjects-list.php")?>" accesskey="s"><i class="fas fa-book"></i> <span>Subjects</span> </a>
+                        <a href="<?=urlOf("pages/subjects/subjects-list.php")?>" accesskey="s"><h5 class="fas fa-book"></h5> <span><h5>Subjects</h5></span> </a>
                      </li>
                      <li class="class">
-                        <a href="<?=urlOf("pages/class/class-list.php")?>" accesskey="c"><i class="fas fa-users-line "></i><span>Class</span></a>
+                        <a href="<?=urlOf("pages/class/class-list.php")?>" accesskey="c"><h5 class="fas fa-users-line "></h5><span><h5>Class</h5></span></a>
                      </li>
                      
    
@@ -32,19 +32,19 @@
                         <span>Management</span>
                      </li>
                      <li class="event">
-                        <a href="<?=urlOf("pages/events/events.php")?>" accesskey="v"><i class="fas fa-calendar-day"></i><span> Events</span></a>
+                        <a href="<?=urlOf("pages/events/events.php")?>" accesskey="v"><h5 class="fas fa-calendar-day"></h5><span> <h5>Events</h5></span></a>
                         
                      </li>
                      <li class="leave">
-                        <a href="<?=urlOf("pages/leave/leave-list.php")?>" accesskey="l"><i class="fas fa-house-user"></i> <span> Leave</span></a>
+                        <a href="<?=urlOf("pages/leave/leave-list.php")?>" accesskey="l"><h5 class="fas fa-house-user"></h5> <span><h5>Leave</h5> </span></a>
                         
                      </li>
                      <li class="Roles">
-                        <a href="<?=urlOf("pages/roles/roles-list.php")?>" accesskey="r"><i class="fas fa-gear"></i> <span> Roles</span></a>
+                        <a href="<?=urlOf("pages/roles/roles-list.php")?>" accesskey="r"><h5 class="fas fa-gear"></h5> <span><h5> Roles</h5></span></a>
                      </li>
 
                      <li class="module">
-                        <a href="<?=urlOf("pages/modules/modules-list.php")?>" accesskey="m"><i class="fas fa-building-lock"></i> <span>Modules</span></a>
+                        <a href="<?=urlOf("pages/modules/modules-list.php")?>" accesskey="m"><h5 class="fas fa-building-lock"></h5> <span><h5>Modules</h5></span></a>
                      </li>
                      
                      </li>
