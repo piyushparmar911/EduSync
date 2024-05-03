f7ccab0e7b0eb578b5ed3eee0b099e055e8f57b9
piyushparmar911 <prparmar911@gmail.com>
2024-05-03 17:07:13 +0530

    index page updated

--- index.php
+++ index.php
@@ -226,28 +226,28 @@ $data = select($query);
                <div class="row">
                   <div class="col-xl-3 col-sm-6 col-12">
                      <div class="card flex-fill fb sm-box">
-                        <i class="fab fa-facebook"></i>
+                        <i class="fab fa-facebook mb-2"></i>
                         <h6>50,095</h6>
                         <p>Likes</p>
                      </div>
                   </div>
                   <div class="col-xl-3 col-sm-6 col-12">
                      <div class="card flex-fill twitter sm-box">
-                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-twitter mb-2"></i>
                         <h6>48,596</h6>
                         <p>Follows</p>
                      </div>
                   </div>
                   <div class="col-xl-3 col-sm-6 col-12">
                      <div class="card flex-fill insta sm-box">
-                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-instagram mb-2"></i>
                         <h6>52,085</h6>
                         <p>Follows</p>
                      </div>
                   </div>
-                  <div class="col-xl-3 col-sm-6 col-12">
+                  <div class="col-xl-3 col-sm-6 col-12 ">
                      <div class="card flex-fill linkedin sm-box">
-                        <i class="fab fa-linkedin-in"></i>
+                        <i class="fab fa-linkedin-in mb-2"></i>
                         <h6>69,050</h6>
                         <p>Follows</p>
                      </div>
